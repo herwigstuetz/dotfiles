1e52aff3501e4620a7e6ea651248eb334a436c03
Herwig Stuetz <herwig@herwigstuetz.com>
2015-12-06 23:05:39 +0100

    Fix creating of symlinks

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 cd $(dirname $0)
 
-ln -s ./profile $HOME/.profile
-ln -s ./bashrc $HOME/.bashrc
-ln -s ./gitconfig $HOME/.gitconfig
-ln -s ./emacs $HOME/.emacs
+ln -rs ./profile $HOME/.profile
+ln -rs ./bashrc $HOME/.bashrc
+ln -rs ./gitconfig $HOME/.gitconfig
+ln -rs ./emacs $HOME/.emacs
