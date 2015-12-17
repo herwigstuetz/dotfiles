0c992f3523698ae9c672410311b0564befe7cc17
Herwig Stuetz <herwig@herwigstuetz.com>
2015-12-17 19:28:43 +0100

    Backup existing dotfiles

--- install.sh
+++ install.sh
@@ -2,6 +2,16 @@
 
 cd $(dirname $0)
 
+# backup old files
+BACKUP=dotfiles.bak
+
+mkdir $BACKUP
+mv $HOME/.profile ./$BACKUP/profile
+mv $HOME/.bashrc ./$BACKUP/bashrc
+mv $HOME/.gitconfig ./$BACKUP/gitconfig
+mv $HOME/.emacs ./$BACKUP/emacs
+
+# install dotfiles
 ln -rs ./profile $HOME/.profile
 ln -rs ./bashrc $HOME/.bashrc
 ln -rs ./gitconfig $HOME/.gitconfig
