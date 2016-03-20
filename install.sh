4ca35b43b69b9cb9716bec6d1171c98c69a13ac1
Herwig Stuetz <herwig@herwigstuetz.com>
2016-03-20 22:34:31 +0100

    Add xmonad

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ mv $HOME/.gitconfig ./$BACKUP/gitconfig
 mv $HOME/.emacs ./$BACKUP/emacs
 mv $HOME/.emacs.d ./$BACKUP/emacs.d
 mv $HOME/.Xdefaults ./$BACKUP/Xdefaults
+mv $HOME/.xmonad ./$BACKUP/xmonad
 
 # install dotfiles
 ln -rs ./profile $HOME/.profile
@@ -19,3 +20,6 @@ ln -rs ./bashrc $HOME/.bashrc
 ln -rs ./gitconfig $HOME/.gitconfig
 ln -rs ./emacs $HOME/.emacs
 ln -rs ./Xdefaults $HOME/.Xdefaults
+
+mkdir $HOME/.xmonad
+ln -rs ./xmonad/xmonad.hs $HOME/.xmonad/xmonad.hs
