33722205d318752d9b26c5779f3249d6cfe46479
Herwig Stuetz <herwig@herwigstuetz.com>
2016-03-23 21:53:19 +0100

    install: Fix xmonad

--- install.sh
+++ install.sh
@@ -21,5 +21,5 @@ ln -rs ./bashrc $HOME/.bashrc
 ln -rs ./gitconfig $HOME/.gitconfig
 ln -rs ./emacs $HOME/.emacs
 ln -rs ./Xdefaults $HOME/.Xdefaults
-ln -rs ./xmonad/ $HOME/.xmonad/
+ln -rs ./xmonad $HOME/.xmonad
 ln -rs ./xmobarrc $HOME/.xmobarrc
