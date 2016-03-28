02f21ec51900adbf96cbffcc1a5c5f308d19a574
Herwig Stuetz <herwig@herwigstuetz.com>
2016-03-28 12:35:20 +0200

    fixup! Add xmobbar

--- install.sh
+++ install.sh
@@ -13,7 +13,6 @@ mv $HOME/.emacs ./$BACKUP/emacs
 mv $HOME/.emacs.d ./$BACKUP/emacs.d
 mv $HOME/.Xdefaults ./$BACKUP/Xdefaults
 mv $HOME/.xmonad ./$BACKUP/xmonad
-mv $HOME/.xmobarrc ./$BACKUP/xmobarrc
 mv $HOME/.xinitrc ./$BACKUP/xinitrc
 mv $HOME/.xsession ./$BACKUP/xsession
 mv $HOME/.compton.conf ./$BACKUP/compton.conf
@@ -25,7 +24,6 @@ ln -rs ./gitconfig $HOME/.gitconfig
 ln -rs ./emacs $HOME/.emacs
 ln -rs ./Xdefaults $HOME/.Xdefaults
 ln -rs ./xmonad $HOME/.xmonad
-ln -rs ./xmobarrc $HOME/.xmobarrc
 ln -rs ./xinitrc $HOME/.xinitrc
 ln -rs ./xsession $HOME/.xsession
 ln -rs ./compton.conf $HOME/.compton.conf
