1291b79713d198646c4135af0428976fb6c4f8c6
Herwig Stuetz <herwig@herwigstuetz.com>
2016-10-15 22:25:12 +0200

    Add xmodmap to switch bkspc and pipe

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ mv $HOME/.gitconfig ./$BACKUP/gitconfig
 mv $HOME/.emacs ./$BACKUP/emacs
 mv $HOME/.emacs.d ./$BACKUP/emacs.d
 mv $HOME/.Xdefaults ./$BACKUP/Xdefaults
+mv $HOME/.Xmodmap ./$BACKUP/Xmodmap
 mv $HOME/.xmonad ./$BACKUP/xmonad
 mv $HOME/.xinitrc ./$BACKUP/xinitrc
 mv $HOME/.xsession ./$BACKUP/xsession
@@ -23,6 +24,7 @@ ln -rs ./bashrc $HOME/.bashrc
 ln -rs ./gitconfig $HOME/.gitconfig
 ln -rs ./emacs $HOME/.emacs
 ln -rs ./Xdefaults $HOME/.Xdefaults
+ln -rs ./Xmodmap $HOME/.Xmodmap
 ln -rs ./xmonad $HOME/.xmonad
 ln -rs ./xinitrc $HOME/.xinitrc
 ln -rs ./xsession $HOME/.xsession
